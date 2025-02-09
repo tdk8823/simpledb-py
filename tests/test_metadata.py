450aa13a672dc1d5662cad689a7e9154cbc6d305
Daisuke Takahashi <57826615+tdk8823@users.noreply.github.com>
2025-02-09 14:14:48 +0900

    implement MetadataManager (#11)

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -5,12 +5,12 @@ from tempfile import TemporaryDirectory
 from simpledbpy.buffer import BufferManager
 from simpledbpy.file import FileManager
 from simpledbpy.log import LogManager
-from simpledbpy.metadata import TableManager
+from simpledbpy.metadata import MetadataManager
 from simpledbpy.record import Schema, Types
 from simpledbpy.tx.transaction import Transaction
 
 
-class TestRecordPage(unittest.TestCase):
+class TestMetadataManager(unittest.TestCase):
     def setUp(self) -> None:
         self.tmp_dir = TemporaryDirectory()
         self.db_directory = Path(self.tmp_dir.name)
@@ -22,25 +22,26 @@ class TestRecordPage(unittest.TestCase):
 
     def test_basic_usage(self) -> None:
         tx = Transaction(self.file_manager, self.log_manager, self.buffer_manager)
-        table_manager = TableManager(is_new=True, tx=tx)
+        metadata_manager = MetadataManager(is_new=True, tx=tx)
 
         schema = Schema()
         schema.add_int_field("A")
         schema.add_string_field("B", 9)
-        table_manager.create_table("MyTable", schema, tx)
 
-        layout = table_manager.get_layout("MyTable", tx)
-        schema_from_table_manager = layout.schema
-        self.assertEqual(schema.fields, schema_from_table_manager.fields)
+        # test table metadata
+        metadata_manager.create_table("MyTable", schema, tx)
+        layout = metadata_manager.get_layout("MyTable", tx)
+        schema_from_metadata_manager = layout.schema
+        self.assertEqual(schema.fields, schema_from_metadata_manager.fields)
         self.assertEqual(
             layout.slot_size, 4 + 4 + 4 + 9
         )  # 4 bytes for flag, 4 bytes for int, 4 bytes for string length, 9 bytes for string
-        for field in schema_from_table_manager.fields:
+        for field in schema_from_metadata_manager.fields:
             if field == "A":
-                self.assertEqual(Types.INTEGER, schema_from_table_manager.type(field))
+                self.assertEqual(Types.INTEGER, schema_from_metadata_manager.type(field))
             elif field == "B":
-                self.assertEqual(Types.VARCHAR, schema_from_table_manager.type(field))
-                self.assertEqual(9, schema_from_table_manager.length(field))
+                self.assertEqual(Types.VARCHAR, schema_from_metadata_manager.type(field))
+                self.assertEqual(9, schema_from_metadata_manager.length(field))
             else:
                 self.fail("Unknown field")
 
